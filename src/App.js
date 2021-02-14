f54ac71f4013356202e201c3b101dde65270741a
Norberg <romainnorberg@gmail.com>
2021-02-14 03:59:14 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
+import { ThemeProvider } from 'styled-components'
+import { LogoIcon } from '@pancakeswap-libs/uikit'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ padding: "32px", width: "400px" }}>
+        <LogoIcon />
+      </div>
     </div>
   );
 }
